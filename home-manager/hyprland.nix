d74a28841a02f4b47d2a35cece06b688d9777a64
pluslatte <debrisfield0201@gmail.com>
2024-12-26 19:41:17 +0900

    add wofi

--- home-manager/hyprland.nix
+++ home-manager/hyprland.nix
@@ -5,9 +5,12 @@
   ...
 }:
 {
+  # Dependancies
   home.packages = with pkgs; [
     xdg-utils
     hyprland-qtutils.packages."${system}".default
+    wofi
+    wofi-emoji
   ];
 
   # You need me!
@@ -40,6 +43,64 @@
       };
     };
   };
+
+  # Launcher wofi config
+  home.file.".config/wofi/style.css".text = ''
+    window {
+      font-family: "FiraCode Nerd Font";
+      font-weight: bold;
+      margin: 0px;
+      border-radius: 10px;
+      background-color: rgba(27, 30, 43, 0.9);
+    }
+
+    #input {
+      all: unset;
+      padding: 2px;
+      border-radius: 6px;
+      margin: 5px;
+      border: none;
+      color: #c8d3f5;
+      background-color: rgba(200, 211, 245, 0.2);
+    }
+
+    #inner-box {
+      margin: 5px;
+      border: none;
+      background-color: transparent;
+    }
+
+    #outer-box {
+      margin: 5px;
+      border: none;
+      background-color: transparent;
+    }
+
+    #scroll {
+      margin: 0px;
+      border: none;
+    }
+
+    #text {
+      margin: 5px;
+      border: none;
+      color: #c8d3f5;
+    }
+
+    #entry {
+      border: none;
+    }
+
+    #entry:focus {
+      border: none;
+    }
+
+    #entry:selected {
+      background-color: rgba(200, 211, 245, 0.2);
+      border-radius: 5px;
+      border: none;
+    }
+  '';
 
   # hyprland
   wayland.windowManager.hyprland = {
