dae15397b4d4d0c1eb97f232690515af48a50207
pluslatte <debrisfield0201@gmail.com>
2025-01-09 14:26:21 +0900

    add alias snrsf for sudo nixos-rebuild switch --flake

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -104,6 +104,10 @@
     EDITOR = "vim";
   };
 
+  programs.bash.shellAliases = {
+    snrsf = "sudo nixos-rebuild switch --flake";
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
